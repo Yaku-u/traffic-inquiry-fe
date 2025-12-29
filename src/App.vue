47777df40488463b51d2a3eb2b091c23b2c77e51
Yaku-u <1816140905@qq.com>
2025-12-29 12:25:48 +0800

    first init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <PathQuery/>
+</template>
+
+<script setup>
+    import PathQuery from './components/PathQuery.vue'
+</script>
+
+<style lang="less" scoped>
+    @import url(./style.less);
+</style>
---
+++ src/components/PathQuery.vue
@@ -0,0 +1,16 @@
+import PathQuery from '@vue/';
+<template>
+    <div class="container">
+        <Menu/>
+        <div class="main"></div>
+        <div class="footer"></div>
+    </div>
+</template>
+
+<script setup>
+    import Menu from './menu.vue'
+</script>
+
+<style lang="less" scoped>
+    @import url(../assets/PathQuery.less);
+</style>
